c8551b234dff78ea90649da694dd7d1bcf9e31e0
matheus <matheusaraujo.dev@gmail.com>
2024-08-15 10:14:55 -0300

    Add: Vue3 frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <AddTodoVue />
+</template>
+
+<script>
+  import { createClient } from '@supabase/supabase-js'
+  const supabaseUrl = 'https://fnzqgervsfmcxdohsvxj.supabase.co'
+  const supabaseKey = process.env.VUE_APP_SUPABASE_KEY;
+  export const supabase = createClient(supabaseUrl, supabaseKey)
+
+  import AddTodoVue from './components/AddTodo.vue';
+
+  const fetchTodos = async () => {
+    let { data: Todos } = await supabase
+      .from('Todos')
+      .select('*')
+
+      console.log(Todos)
+  }
+
+  const mounted = () => {
+    fetchTodos();
+  }
+
+export default {
+  name: 'App',
+  components: {
+    AddTodoVue,
+  },
+  mounted,
+  methods: {
+    fetchTodos,
+  }
+}
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ frontend/src/components/AddTodo.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>ADD TODO FORM</h1>
+</template>
+
+<script>
+export default {
+  name: 'AddTodo',
+}
+</script>
+
+<style>
+
+</style>
